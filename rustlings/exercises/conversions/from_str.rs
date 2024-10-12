665a8f32b5f98d2dfc487bac4ddfca8f699dc55c
Dan1cos <art.sha03@gmail.com>
2024-10-12 20:25:05 +0200

    Completed clippy, conversions and advanced errors. All tasks are completed

--- rustlings/exercises/conversions/from_str.rs
+++ rustlings/exercises/conversions/from_str.rs
@@ -26,7 +26,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+// I AM DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -41,6 +41,20 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let splitted: Vec<&str> = s.split(',').collect();
+        if splitted.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if splitted[0] == "" {
+            return Err(ParsePersonError::NoName);
+        }
+        match splitted[1].parse::<usize>(){
+            Ok(x) => Ok(Person{name: String::from(splitted[0]), age: x}),
+            Err(err) => Err(ParsePersonError::ParseInt(err))
+        }
     }
 }
 
